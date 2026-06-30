08c16dd39cfd3dc15bdb96641c0ebf9ad0aeb652
sanitta2002 <sanittajohnson28@gmail.com>
2026-06-30 20:50:40 +0530

    fix: refactor MailService to use service configuration for nodemailer

--- Backend/src/infrastructure/services/MailService.ts
+++ Backend/src/infrastructure/services/MailService.ts
@@ -1,27 +1,22 @@
 import { IMailService } from "@/domain/interfaces/services/IMailService";
 import { injectable } from "tsyringe";
-import * as nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
 @injectable()
 export class MailService implements IMailService {
-  private transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  async sendResetPasswordEmail(
-    email: string,
-    resetLink: string,
-  ): Promise<void> {
-    await this.transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Password Reset Request",
-      html: `
+    private transporter =nodemailer.createTransport({
+       service: "gmail",
+       auth: {
+         user: process.env.EMAIL_USER,
+         pass: process.env.EMAIL_PASSWORD,
+       },
+    });
+    async sendResetPasswordEmail(email: string, resetLink: string): Promise<void> {
+        await this.transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Password Reset Request",
+            html: `
         <h2>Password Reset Request</h2>
         <p>You requested a password reset.</p>
         <p>Click the button below to reset your password.</p>
@@ -40,6 +35,6 @@ export class MailService implements IMailService {
         <p>If you didn't request this, you can safely ignore this email.</p>
         <p>This link expires in <strong>15 minutes</strong>.</p>
       `,
-    });
-  }
+        });
+    }
 }
