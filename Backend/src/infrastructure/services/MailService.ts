21ed7e39eaea59efce87e18e721f56f5fed26605
sanitta2002 <sanittajohnson28@gmail.com>
2026-06-30 20:15:37 +0530

    fix: update MailService to use SMTP configuration for Gmail

--- Backend/src/infrastructure/services/MailService.ts
+++ Backend/src/infrastructure/services/MailService.ts
@@ -5,7 +5,9 @@ import nodemailer from "nodemailer";
 @injectable()
 export class MailService implements IMailService {
   private transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
